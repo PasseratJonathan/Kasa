4e921461af7eccf4061f9d54a8f16920f986a3bc
Jonathan Passerat <jonathansylar@hotmail.fr>
2023-06-17 19:23:42 +0200

    fin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import reportWebVitals from './reportWebVitals'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
@@ -9,7 +9,7 @@ import APropos from './pages/A-Propos'
 import Error from './pages/Error'
 
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
@@ -20,7 +20,6 @@ ReactDOM.render(
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
 )
 
 reportWebVitals();
